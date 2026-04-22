b736fbd325ab2890c5a634d9ba5a2c2ca3c82346
Iran Marley <iran_marley@live.com>
2026-04-22 17:09:44 +0200

    fix: swagger check

--- src/Ims.YamiFlow.API/Extensions/ProgramExtensions.cs
+++ src/Ims.YamiFlow.API/Extensions/ProgramExtensions.cs
@@ -33,8 +33,8 @@ public static class ProgramExtensions
         {
             app.UseHttpsRedirection();
         }
-        else
-        {
+        // else
+        // {
             // Swagger
             app.UseSwagger();
             app.UseSwaggerUI(opt =>
@@ -42,7 +42,7 @@ public static class ProgramExtensions
                 opt.SwaggerEndpoint("/swagger/v1/swagger.json", "YamiFlow API v1");
                 opt.RoutePrefix = "swagger";
             });       
-        }
+        //}
 
         app.UseOpenTelemetryPrometheus();
         app.UseSerilogRequestLogging();
